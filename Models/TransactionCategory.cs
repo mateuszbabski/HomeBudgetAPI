589580c94cdcf1828c1eb4c4e6e40d26d6aa78f5
mateuszbabski <babskimateusz@gmail.com>
2022-05-19 09:22:27 +0200

    Categorize expenses

---
+++ Models/TransactionCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeBudget.Models
+{
+    public class TransactionCategory
+    {
+        public const string Job = "Job";
+        public const string Investments = "Investments";
+        public const string Fuel = "Fuel";
+        public const string Groceries = "Groceries";
+        public const string Mortgage = "Mortgage";
+        public const string Travel = "Travel";
+        public const string Bills = "Bills";
+        public const string Alcohol = "Alcohol";
+        public const string Accessories = "Accessories";
+        public const string Personal = "Personal";
+        public const string Healthare = "Healthcare";
+        public const string Others = "Others";
+    }
+}
---
+++ Models/TransactionType.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeBudget.Models
+{
+    public class TransactionType
+    {
+        public const string Income = "Income";
+        public const string Expense = "Expense";
+        public const string Savings = "Savings";
+    }
+}
